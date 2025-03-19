cdd3b68c58d78530645f25ac1aaaec8a191d823b
WojtAcht <wachtelik@gmail.com>
2025-03-19 19:41:37 +0100

    feat: add R3 and DE for 2D

--- src/adaptations/adaptations_2D/__init__.py
+++ src/adaptations/adaptations_2D/__init__.py
@@ -1,7 +1,11 @@
 from .density_sampling import DensitySamplingAdaptation2D
 from .no_adaptation import NoAdaptation2D
+from .r3 import R3Adaptation2D
+from .de import DEAdaptation2D
 
 __all__ = [
     "DensitySamplingAdaptation2D",
     "NoAdaptation2D",
+    "R3Adaptation2D",
+    "DEAdaptation2D",
 ]
